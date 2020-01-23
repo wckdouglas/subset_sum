cdfeeb18dd645786da8189a2dcfd70786851bde9
Douglas Wu <douglas.wu@qiagen.com>
2020-01-23 10:58:46 -0500

    used cython

---
+++ src/subset_sum.pyx
@@ -0,0 +1,34 @@
+from flask import flash
+
+cdef subset_sum(numbers, target, partial=[]):
+    '''
+    https://stackoverflow.com/questions/4632322/finding-all-possible-combinations-of-numbers-to-reach-a-given-sum
+    '''
+
+    cdef:
+        int i
+        double n
+        double s
+
+
+    try:
+        numbers = list(map(float, numbers))
+    except ValueError:
+        return 'Some of the input numbers are not valid numbers'
+
+    try:
+        target = float(target)
+    except ValueError:
+        return 'Target sum number is not a valid number'
+
+    s = sum(partial)
+
+    # check if the partial sum is equals to target
+    if s == target: 
+        flash("sum(%s)=%s" % (partial, target))
+        return 0
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        remaining = numbers[i+1:]
+        subset_sum(remaining, target, partial + [n]) 
